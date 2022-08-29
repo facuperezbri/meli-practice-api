bc309139b529e805296c8936cf406aa98827fc19
facuperezbri <facundo.perez.brizuela@gmail.com>
2022-08-29 13:09:31 -0300

    update

---
+++ index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var morgan = require('morgan');
+const axios = require('axios')
+
+var app = express();
+const PORT = process.env.PORT || 3001;
+var cors = require('cors');
+const { restart } = require('nodemon');
+
+app.use(morgan('dev')) // Morgan recibe tipo de config
+app.use(express.json()); // Bodyparser a JSON
+app.use(cors());
+
+app.listen(PORT, () => {
+  console.log(`Running on http://localhost:${PORT}`)
+});
+
+app.get('/items', async function (req, res) {
+  const { name } = req.query
+  const data = await axios.get(`https://api.mercadolibre.com/sites/MLA/search?q=${name}`)
+  res.send(data.data)
+})
+
+app.get('/items/:id', async function (req, res) {
+  const { id } = req.params
+  let data = await axios.get(`https://api.mercadolibre.com/items/${id}`)
+  res.send(data.data)
+})
